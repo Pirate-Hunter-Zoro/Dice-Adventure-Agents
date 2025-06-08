9c4dc9bb50cf7ac6dcf20375e142cac24384f5dc
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2025-06-08 18:41:15 -0500

    began agent implementation

--- agent_template/agent.py
+++ agent_template/agent.py
@@ -22,6 +22,49 @@ class DiceAdventureAgent:
         """
         self.character = character_name
         self.character_id = character_id
+        self.has_key = False
+
+        if self.is_dwarf():
+            self.action_points = 6
+            self.vision_range = 3
+            self.key_shrine = 'K1'
+            self.type = 'Dwarf'
+            self.skills = {'rocks': 4, 'traps': 8, 'monsters': 6}
+        elif self.is_human():
+            self.action_points = 4
+            self.vision_range = 5
+            self.key_shrine = 'K3'
+            self.type = 'Human'
+            self.skills = {'rocks': 6, 'traps': 6, 'monsters': 8}
+        elif self.is_giant():
+            self.action_points = 3
+            self.vision_range = 7
+            self.key_shrine = 'K2'
+            self.type = 'Giant'
+            self.skills = {'rocks': 8, 'traps': 4, 'monsters': 6}
+        else:
+            assert False, "Invalid character ID. Must be C11, C21, or C31."
+
+    def is_dwarf(self) -> bool:
+        """
+        Check if the agent is playing as a dwarf.
+        :return: True if the agent is a dwarf, False otherwise.
+        """
+        return self.character_id == "C11"
+    
+    def is_giant(self) -> bool:
+        """
+        Check if the agent is playing as a giant.
+        :return: True if the agent is a giant, False otherwise.
+        """
+        return self.character_id == "C21"
+    
+    def is_human(self) -> bool:
+        """
+        Check if the agent is playing as a human.
+        :return: True if the agent is a human, False otherwise.
+        """
+        return self.character_id == "C31"
 
     def take_action(self, state: list[dict], actions: list[str]) -> str:
         """
@@ -31,4 +74,79 @@ class DiceAdventureAgent:
         :param actions: A list of string action names
         :return:        An action from the 'actions' list
         """
-        raise NotImplementedError("Not implemented yet.")
+        my_position = None
+        shrine_position = None
+        tower_position = None
+        visible_monsters = []
+        visible_rocks = []
+        visible_traps = []
+        for entity in state:
+            entityType = entity.get('entityType', None)
+            if entityType == None:
+                entityType = entity.get('type', None)
+            objKey = entity.get('objKey', None)
+
+            if entityType == self.type:
+                my_position = (entity.get('x'), entity.get('y'))
+            elif entityType == 'monster':
+                visible_monsters.append(entity)
+            elif entityType == 'Shrine' and entity.get('character') == self.type:
+                shrine_position = (entity.get('x'), entity.get('y'))
+            elif entityType == 'Goal':
+                tower_position = (entity.get('x'), entity.get('y'))
+            elif objKey == 'R1':
+                visible_rocks.append(entity)
+            elif objKey == 'T1':
+                visible_traps.append(entity)
+        
+        if my_position and shrine_position:
+            self.has_key = my_position == shrine_position
+
+        ping_string = None
+        if visible_monsters and not self.is_human():
+            # If there are visible monsters, and I'm not the human, I should not attack but instead ping
+            first_monster = visible_monsters[0]
+            ping_string = f'ping ! {first_monster.get("x")} {first_monster.get("y")}'
+        elif visible_rocks and self.is_dwarf():
+            # Gotta ping the rocks if I'm a dwarf because I'm bad at them
+            first_rock = visible_rocks[0]
+            ping_string = f'ping V {first_rock.get("x")} {first_rock.get("y")}'
+        elif visible_traps and self.is_giant():
+            # Gotta ping the traps if I'm a giant because I'm bad at them
+            first_trap = visible_traps[0]
+            ping_string = f'ping V {first_trap.get("x")} {first_trap.get("y")}'
+
+        if not ping_string:
+            # If I didn't ping, then I need to do something else
+            if visible_monsters:
+                assert self.is_human(), "If a monster was visible and I was not human, I should have pinged."
+                first_monster = visible_monsters[0]
+                return f'move {first_monster.get("x")} {first_monster.get("y")}'
+            elif visible_traps:
+                assert not self.is_giant(), "If a trap was visible and I was a giant, I should have pinged."
+                first_trap = visible_traps[0]
+                return f'move {first_trap.get("x")} {first_trap.get("y")}'
+            elif visible_rocks:
+                assert not self.is_dwarf(), "If a rock was visible and I was a dwarf, I should have pinged."
+                first_rock = visible_rocks[0]
+                return f'move {first_rock.get("x")} {first_rock.get("y")}'
+            else:
+                # Goal seeking state
+                assert my_position, "I should have a position."
+                if self.has_key and tower_position:
+                    # Goal is tower and tower is visible
+                    return f'move {tower_position[0]} {tower_position[1]}'
+                elif not self.has_key and shrine_position:
+                    # Goal is shrine and shrine is visible
+                    return f'move {shrine_position[0]} {shrine_position[1]}'
+                elif self.has_key:
+                    # Goal is tower but tower is not visible
+                    # TODO - will need to implement a way to get the tower visible
+                    return 'wait'
+                else: 
+                    # Goal is shrine but shrine is not visible
+                    # TODO - will need to implement a way to get the shrine visible
+                    return 'wait'
+        else:
+            # If I pinged, then I need to return the ping string
+            return ping_string
